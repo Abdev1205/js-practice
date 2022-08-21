30eff8905d7caf52b1d1cdb4b011c37e42a26a21
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2022-08-22 00:14:52 +0530

    1st day of hard work

---
+++ searching in array/index_of.js
@@ -0,0 +1,13 @@
+/*  1. Why do we need index of method in array searching
+----> it used foe finding index of searching element
+
+ */
+//ex 
+
+let subject =['Ei','HCI','Differential','Discrete','COA','DLD']
+console.log(subject.indexOf("COA"));
+console.log(subject.indexOf('COA',5));  // giving parameter from which index value it should searching 
+
+/* ----> IMP if output is -1 then it means no result found
+  -----> It is case sensitivity
+*/

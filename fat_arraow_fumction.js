8e803cffe788cf1b6ed7642c747a73f3bb31a567
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2022-08-21 18:17:55 +0530

    nothing special

---
+++ fat_arraow_fumction.js
@@ -0,0 +1,8 @@
+// this fat arrow function is really vardaan of god
+// bhai sahab ekdam bade code ko chota kar deta hai
+// isme function keyword nahi likhna padta hai only => a lagayo aur apna kaam karo 
+
+const sum = (a,b) =>(`sum of ${(a=2)} and ${(b=4)} = ${a+b}`);
+
+
+console.log(sum());

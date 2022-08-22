c0cc8a6ab206641cfa7f50e8d0315fe4ccd8b37b
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2022-08-22 23:26:20 +0530

    2nd day over ,I now very low speed for practice but i increase my speed in future

---
+++ array/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+/*  1. Why do we need push() method in array crud operation
+----> it is used to add one or more element at the end of the array 
+----> it return new length of array 
+
+ */
---
+++ string/string.js
@@ -0,0 +1,15 @@
+// A JavaScript string is zero or more characters written inside quotes.
+
+// JavaScript strings are used for storing and manipulating text.
+// You can use single or double quotes
+
+// Strings can be created as primitives, 
+// from string literals, or as objects, using the String() constructor
+
+//ex
+
+myName="Abhay Mishra";
+
+
+console.log(myName); 
+console.log(`Length of ${myName} is ${myName.length}`);      // here space is also count as length of string      
